d8ee5e8ee344e6e078891a48f5c378ba62067d56
Neicer Vásquez <NeicerYEANYT18@gmail.com>
2024-01-15 17:19:25 -0500

    Agregando funciones en el archivo tools

--- tools.py
+++ tools.py
@@ -1,1 +1,17 @@
 print('Estoy aprendiendo muy rápido 😎')
+
+x = 1
+def too():
+    print(x.is_integer())
+
+def too1():
+    '''
+    El operador de asignación +=
+    trabaja en un área local y no
+    de manera global
+    '''
+    x+=1
+    print(x)
+
+too()
+too1()
