208f7d4b570b25300e2533c4f4899ab247e88973
Neicer Vásquez <NeicerYEANYT18@gmail.com>
2024-01-29 11:40:21 -0500

    Lectura y Escritura en pruebas.py sobre el test.txt

--- practica/pruebas.py
+++ practica/pruebas.py
@@ -8,3 +8,8 @@ print({1,2} & {2,4})
 # 0000
 names = {'Nicolas', 'Miguel', 'Juan', 'Nicolas'} 
 print(names)
+
+with open('./practica/test.txt', 'a+') as file:
+    file.write('Hola Python, aun te recuerdo\n')
+    file.seek(0)
+    print(file.read())
