5126fdcf17c1d6d165a52fea017cf106994be1ac
Neicer_VB <147959257+NeicerVB@users.noreply.github.com>
2024-01-29 22:47:56 -0500

    Verifica si el usuario está en la lista de usuarios verificados

--- login.py
+++ login.py
@@ -3,3 +3,10 @@ print('He aprendido a crear una rama en git')
 frase = 'Mi gata se llama Kukis y tiene 10 años'
 frase1 = "".join(str(i) for i in range(5))
 print(frase1)
+
+nombre = input('¿Cuál es tu nombre?: ')
+listUsers = list('Neicer', 'Vicente', 'Maria', 'Milenka', 'Leo', 'Mishell', 'Bryan')
+if nombre in listUsers:
+  print('Welcome to NIVER')
+else:
+  print('You don\'t belong to the service')
