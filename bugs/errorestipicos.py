d92ec05e9661a78ed1651b22c1c63cc14467e5d0
Neicer Vásquez <NeicerYEANYT18@gmail.com>
2024-01-31 13:32:19 -0500

    Se indica un nuevo tipo de error común, NO se puede renombrar un elemento de un string aunque se acceda a dicho elemento por medio de su indice

--- bugs/errorestipicos.py
+++ bugs/errorestipicos.py
@@ -2,3 +2,8 @@
 file = open('./Carta.txt', 'r+')
 file.read() # Lee el archivo
 file.close() # cierra el archivo
+
+# Error tipico, los elementos de un strin son inmutables
+nombre = input('Ingrese su nombre: ')
+nombre[0] = 'e'
+print(nombre)
